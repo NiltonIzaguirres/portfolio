59b3a8cae613c83a20b6e8e360b2c123c10fb2d5
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-03 20:17:48 -0300

    feat: swapped favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
