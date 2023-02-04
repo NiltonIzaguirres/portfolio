80aa412e152b922a134088ae194c28602d778e6c
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-04 12:00:52 -0300

    feat: created Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,176 @@
-import { useThemeMode } from "@/context/ThemeModeContext";
-import styled from "styled-components";
-
-const Text = styled.h1`
-  color: ${(props) => props.theme.colors.text};
-`;
+import { homeTranslate } from "@/translate/texts";
+import NextImage from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { BsChevronDoubleDown, BsGithub, BsLinkedin } from "react-icons/bs";
+import {
+  SiJavascript,
+  SiTypescript,
+  SiNextdotjs,
+  SiReact,
+  SiHtml5,
+  SiCss3,
+  SiNodedotjs,
+  SiPython,
+  SiVisualstudiocode,
+  SiFigma,
+  SiInsomnia,
+  SiAdobephotoshop,
+} from "react-icons/si";
+import { Card } from "@/components/Card";
+import { Category } from "@/components/Category";
+import { SEO } from "@/components/SEO";
+import { SubTitle } from "@/components/SubTitle";
+import {
+  AboutMeSection,
+  ContactInfo,
+  ContactSection,
+  Main,
+  ScrollDown,
+  SkillsSection,
+  WelcomeSection,
+} from "@/styles/pages/Home";
+import { IoLocationOutline, IoMailOutline } from "react-icons/io5";
+import { FramedIcon } from "@/components/FramedIcon";
 
 export default function Home() {
-  const { toggleTheme } = useThemeMode();
+  const { locale } = useRouter();
+
+  const currentLocale = locale === "en-US" ? "en-US" : "pt-BR";
+  const translations = homeTranslate[currentLocale];
+
   return (
     <>
-      <Text>Hello Word!</Text>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <SEO
+        title={translations.seo.title}
+        description={translations.seo.description}
+        routeName={"/"}
+      />
+
+      <Main>
+        <WelcomeSection>
+          <div>
+            <p>{translations.welcomeSection.hello}</p>
+            <h1>Nilton Izaguirres.</h1>
+            <p>{translations.welcomeSection.resume}</p>
+            <Link href="#contact" scroll={false}>
+              {translations.welcomeSection.contact}
+            </Link>
+          </div>
+
+          <NextImage
+            src="/images/me.png"
+            alt={translations.welcomeSection.imageAlt}
+            width={395}
+            height={658}
+          />
+          <ScrollDown>
+            <Link href="#about" scroll={false}>
+              <BsChevronDoubleDown />
+            </Link>
+          </ScrollDown>
+        </WelcomeSection>
+
+        <AboutMeSection id="about">
+          <NextImage
+            src="https://github.com/niltonizaguirres.png"
+            alt={translations.aboutSection.imageAlt}
+            style={{ objectFit: "cover" }}
+            width={395}
+            height={500}
+          />
+
+          <div>
+            <SubTitle title={translations.aboutSection.title} />
+            {translations.aboutSection.descriptions.map(
+              (description, index) => (
+                <p key={index}>{description}</p>
+              )
+            )}
+          </div>
+        </AboutMeSection>
+
+        <SkillsSection>
+          <SubTitle title={translations.skillsSection.title} />
+
+          <Category
+            title={translations.skillsSection.categories.main.title}
+            warn={translations.skillsSection.categories.main.warn}
+          >
+            <Card icon={<SiJavascript />} title="JavaScript" />
+            <Card icon={<SiTypescript />} title="TypeScript" />
+            <Card icon={<SiNextdotjs />} title="Next.JS" />
+            <Card icon={<SiReact />} title="React" />
+            <Card icon={<SiHtml5 />} title="HTML 5" />
+            <Card icon={<SiCss3 />} title="CSS" />
+            <Card icon={<SiNodedotjs />} title="Node.JS" />
+            <Card icon={<SiPython />} title="Python" />
+          </Category>
+          <Category
+            title={translations.skillsSection.categories.tools.title}
+            warn={translations.skillsSection.categories.main.warn}
+          >
+            <Card icon={<SiVisualstudiocode />} title="VS Code" />
+            <Card icon={<SiFigma />} title="Figma" />
+            <Card icon={<SiInsomnia />} title="Insomnia" />
+            <Card icon={<SiAdobephotoshop />} title="PhotoShop" />
+          </Category>
+        </SkillsSection>
+
+        <ContactSection id="contact">
+          <SubTitle title={translations.contactSection.title} />
+
+          <Category
+            title={translations.contactSection.categories.contact.title}
+            warn={translations.contactSection.categories.contact.description}
+          >
+            <ContactInfo>
+              <FramedIcon icon={<IoLocationOutline />} />
+              <span>
+                Florianópolis, Santa catarina,{" "}
+                {translations.contactSection.categories.contact.country}
+              </span>
+            </ContactInfo>
+            <ContactInfo>
+              <FramedIcon icon={<IoMailOutline />} />
+              <span>
+                <a href="mailto:niltonizaguirres2003@gmail.com">
+                  niltonizaguirres2003@gmail.com
+                </a>
+              </span>
+            </ContactInfo>
+          </Category>
+
+          <Category
+            title={translations.contactSection.categories.mediaSocial.title}
+          >
+            <ContactInfo>
+              <FramedIcon icon={<BsLinkedin />} />
+              <span>
+                <a
+                  href="https://www.linkedin.com/in/nilton-izaguirres/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Linkedin
+                </a>
+              </span>
+            </ContactInfo>
+            <ContactInfo>
+              <FramedIcon icon={<BsGithub />} />
+              <span>
+                <a
+                  href="https://github.com/NiltonIzaguirres/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </span>
+            </ContactInfo>
+          </Category>
+        </ContactSection>
+      </Main>
     </>
   );
 }
---
+++ src/styles/pages/Home.tsx
@@ -0,0 +1,170 @@
+import styled, { css } from "styled-components";
+
+export const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  max-width: 1280px;
+  margin: auto;
+  padding: 0 2rem;
+`;
+
+export const WelcomeSection = styled.section`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: calc(100vh - 4rem);
+  width: 100%;
+  padding: 0.5rem 0;
+  color: ${(props) => props.theme.colors.textHighlight};
+
+  & > div:first-of-type {
+    max-width: 42rem;
+
+    p:first-child {
+      font-weight: 600;
+      font-size: 1.7rem;
+    }
+
+    h1 {
+      font-weight: bold;
+      font-size: 3.7rem;
+    }
+
+    p:nth-child(3) {
+      font-size: 1.2rem;
+      font-weight: 500;
+      margin-bottom: 3rem;
+      color: ${(props) => props.theme.colors.text};
+    }
+
+    a {
+      border-radius: 8px;
+      padding: 0.5rem 1.5rem;
+      font-size: 1.5rem;
+      font-weight: 600;
+
+      ${(props) => css`
+        background-color: ${props.theme.colors.secondary};
+        color: ${props.theme.colors.buttonText};
+      `};
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+  }
+
+  & > img {
+    border-radius: 8px;
+    border: 4px solid ${(props) => props.theme.colors.secondary};
+  }
+`;
+
+export const ScrollDown = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: calc(50% - 25px);
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & > a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    color: ${(props) => props.theme.colors.textHighlight};
+  }
+`;
+
+export const AboutMeSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+
+  & > div {
+    max-width: 55%;
+
+    & > h2 {
+      margin-bottom: 2rem;
+    }
+
+    & > p {
+      font-size: 1.3rem;
+      font-weight: 500;
+      line-height: 2rem;
+      text-indent: 3rem;
+    }
+
+    p + p {
+      margin-top: 0.5rem;
+    }
+  }
+
+  & > img {
+    border-radius: 8px;
+    border: 4px solid ${(props) => props.theme.colors.secondary};
+  }
+`;
+
+export const SkillsSection = styled.section`
+  margin-top: 4rem;
+
+  & > h2 {
+    margin-bottom: 4rem;
+  }
+
+  & > section {
+    margin-bottom: 2rem;
+  }
+`;
+
+export const ContactSection = styled.section`
+  & > h2 {
+    margin-bottom: 4rem;
+  }
+
+  & > section {
+    margin-bottom: 2rem;
+  }
+`;
+
+export const ContactInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 0.8rem;
+
+  span {
+    margin-left: 1rem;
+    font-weight: 500;
+    font-size: 1.5rem;
+
+    a {
+      position: relative;
+      transition: 0.2 all;
+
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 0;
+        bottom: -3px;
+        height: 3px;
+        transition: 0.3s width;
+        border-radius: 8px;
+        background-color: ${(props) => props.theme.colors.secondary};
+      }
+
+      &:hover {
+        &::after {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
