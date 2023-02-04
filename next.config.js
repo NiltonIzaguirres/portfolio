f38e99b76acf05bb65c0524c77615cb8df56e80e
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-03 23:57:25 -0300

    feat: added domain for github image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['github.com']
+  },
   i18n: {
     locales: ['en-US', 'pt-BR'],
     defaultLocale: 'en-US',
