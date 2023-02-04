7484ecd76828fb0c63a5eb11f1842578646b08ae
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-04 12:00:25 -0300

    feat: added Home page translate

--- src/translate/texts.ts
+++ src/translate/texts.ts
@@ -11,11 +11,95 @@ export const headerTranslate = {
 
 export const homeTranslate = {
   "en-US": {
-    title: 'Home',
-    articles: 'Articles'
+    seo: {
+      title: 'Home',
+      description: 'A young full-stack developer and technology, hacking and design enthusiast'
+    },
+    welcomeSection: {
+      hello: 'Hi, i am',
+      resume: 'a young full-stack developer passionate about solving problems and creating solutions!',
+      contact: 'Get in touch',
+      imageAlt: 'My image in drawing version'
+    },
+    aboutSection: {
+      title: "About me",
+      descriptions: [
+        "Pleasure my name is Nilton and I'm currently 19 years old, with only 15 years old I started learning hacking, but I fell in love with creating solutions and solving problems right when I started learning my first programming language, from that moment I started to learn and develop myself each more in software development.",
+        "I'm currently studying for a bachelor's degree in computer science, in addition to specializing more and more as a software developer and always looking to learn more."
+      ],
+      imageAlt: "My Github Image"
+    },
+    skillsSection: {
+      title: "My skills",
+      categories: {
+        main: {
+          title: "Main technologies",
+          warn: "These are just some technologies that I have knowledge, if you want to know more please contact me or take a look at my projects."
+        },
+        tools: {
+          title: "Tools",
+          warn: "These are some tools that I am aware of, if you want to know more, please contact me."
+        },
+      }
+    },
+    contactSection: {
+      title: "Contact",
+      categories: {
+        contact: {
+          title: 'Contact',
+          description: "If you need more information, for example my phone number, please don't hesitate to email me first.",
+          country: 'Brazil'
+        },
+        mediaSocial: {
+          title: 'Social networks'
+        }
+      }
+    }
   },
   "pt-BR": {
-    about: 'Sobre',
-    articles: 'Artigos'
+    seo: {
+      title: 'Inicio',
+      description: 'Um jovem desenvolvedor full-stack e entusiasta de tecnologia, hacking e design'
+    },
+    welcomeSection: {
+      hello: 'Olá, sou',
+      resume: 'um jovem desenvolvedor full-stack apaixonado por resolver problemas e criar soluções!',
+      contact: 'Entrar em contato',
+      imageAlt: 'Minha imagem em versão de desenho'
+    },
+    aboutSection: {
+        title: "Sobre mim",
+        descriptions: [
+          "Prazer meu nome é Nilton e atualmente tenho 19 anos, com apenas 15 anos comecei aprendendo hacking, mas me apaixonei por criar soluções e resolver problemas logo quando comecei a aprender minha primeira linguagem de programação, a partir desde momento comecei a aprender e me desenvolver cada vez mais em desenvolvimento de software.",
+          "Atualmente estou cursando bacharel em ciência da computação, além de estar me especializando cada vez mais como desenvolvedor de software e estar sempre buscando aprender mais."
+        ],
+        imageAlt: "Minha imagem do Github"
+    },
+    skillsSection: {
+      title: "Minhas habilidades",
+      categories: {
+        main: {
+          title: "Principais tecnologias",
+          warn: "Essas são apenas algumas tecnologias que tenho conhecimentos, caso queira conhecer mais entre em contato comigo ou dê uma olhada em meus projetos."
+        },
+        tools: {
+          title: "Ferramentas",
+          warn: "Essas são algumas ferramentas que tenho conhecimento, caso queira saber mais entre em contato comigo."
+        },
+      }
+    },
+    contactSection: {
+      title: "Contato",
+      categories: {
+        contact: {
+          title: 'Contato',
+          description: 'Se precisar de mais informações, por exemplo, meu número de telefone, não hesite em me enviar um e-mail primeiro.',
+          country: 'Brasil'
+        },
+        mediaSocial: {
+          title: 'Redes sociais'
+        }
+      }
+    }
   }
 }
