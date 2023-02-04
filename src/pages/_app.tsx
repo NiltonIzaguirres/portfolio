5ebd9beaad9826f37cef03ce50acb46318f92f49
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-04 19:53:46 -0300

    feat: header and footer do not appear on page 404

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,19 @@ import { ThemeModeProvider } from "@/context/ThemeModeContext";
 import { Header } from "@/components/Header";
 import { GlobalStyles } from "@/styles/global";
 import { Footer } from "@/components/Footer";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter();
+
+  const isErrorPage = asPath === "/404";
+
   return (
     <ThemeModeProvider>
-      <Header />
+      {isErrorPage || <Header />}
       <GlobalStyles />
       <Component {...pageProps} />
-      <Footer />
+      {isErrorPage || <Footer />}
     </ThemeModeProvider>
   );
 }
