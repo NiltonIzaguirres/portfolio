dc9f8c3b36dc8049c2bb57bedd5539bc28d47d0d
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-07 16:08:50 -0300

    feat: created ProjectDisplay component

---
+++ src/components/ProjectDisplay/index.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import { ImLink } from "react-icons/im";
+import { SiGithub } from "react-icons/si";
+import {
+  LinksContainer,
+  InfoContainer,
+  ProjectDisplayContainer,
+  TechContainer,
+} from "./styles";
+
+interface ProjectCardTranslationProps {
+  tech: string;
+  github: string;
+  preview: string;
+}
+
+interface ProjectDisplayProps {
+  name: string;
+  slug: string;
+  description: string;
+  techs: string[];
+  isGithub: boolean;
+  preview: string;
+  projectCardTranslation: ProjectCardTranslationProps;
+}
+
+export function ProjectDisplay({
+  name,
+  slug,
+  description,
+  techs,
+  isGithub,
+  preview,
+  projectCardTranslation,
+}: ProjectDisplayProps) {
+  return (
+    <ProjectDisplayContainer>
+      <Image
+        src={`/images/projects/${slug}.png`}
+        width={265}
+        height={200}
+        style={{ objectFit: "cover" }}
+        alt={slug}
+      />
+      <InfoContainer>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </InfoContainer>
+      <TechContainer>
+        <h4>{projectCardTranslation.tech}</h4>
+        <p>{techs}</p>
+      </TechContainer>
+      <LinksContainer>
+        {isGithub && (
+          <a
+            href={`https://github.com/niltonizaguirres/${slug}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiGithub />
+            <span>{projectCardTranslation.github}</span>
+          </a>
+        )}
+        {preview && (
+          <a href={preview} target="_blank" rel="noreferrer">
+            <ImLink /> <span>{projectCardTranslation.preview}</span>
+          </a>
+        )}
+      </LinksContainer>
+    </ProjectDisplayContainer>
+  );
+}
---
+++ src/components/ProjectDisplay/styles.ts
@@ -0,0 +1,90 @@
+import styled, {css} from "styled-components";
+
+export const ProjectDisplayContainer = styled.div`
+  width: 280px;
+  height: 420px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.7rem;
+  border-radius: 4px;
+  background: ${props => props.theme.colors.backgroundHighlight};
+  box-shadow: 0 10px 20px rgba(0,0,0,0.4);
+  overflow: hidden;
+
+  img {
+    border-radius: 4px;
+  }
+`
+
+export const InfoContainer = styled.div`
+  width: 100%;
+  margin: 0.8rem 0;
+
+  h3 {
+    color: ${props => props.theme.colors.textHighlight};
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    width: 100%;
+    height: 37px;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+`;
+
+export const TechContainer = styled.div`
+  width: 100%;
+  margin-bottom: 0.8rem;
+
+  h4 {
+    color: ${props => props.theme.colors.text};
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    width: 100%;
+    height: 37px;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+`;
+
+export const LinksContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${props=> props.theme.colors.secondary};
+    color: ${props=> props.theme.colors.buttonText};
+    padding: 0.5rem;
+    font-size: 0.9rem;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: 0.2s all;
+
+    span {
+      margin-left: 0.5rem;
+    }
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
