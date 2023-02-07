bb1ecb57c986290ce1148661b808b96b2c977a06
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-07 16:09:29 -0300

    feat: created tech tag list

---
+++ src/shared/techTags.tsx
@@ -0,0 +1,73 @@
+import {
+  SiAuthy,
+  SiExpo,
+  SiExpress,
+  SiNextdotjs,
+  SiPrismic,
+  SiReact,
+  SiSass,
+  SiSqlite,
+  SiStripe,
+  SiStyledcomponents,
+} from "react-icons/si";
+import { ImDatabase } from "react-icons/im";
+import { MdQueryBuilder, MdOutlineGetApp } from "react-icons/md";
+
+export const techTags = [
+  {
+    value: "React.JS",
+    icon: <SiReact />,
+  },
+  {
+    value: "React Native",
+    icon: <SiReact />,
+  },
+  {
+    value: "Axios",
+    icon: <MdOutlineGetApp />,
+  },
+  {
+    value: "Expo",
+    icon: <SiExpo />,
+  },
+  {
+    value: "Express",
+    icon: <SiExpress />,
+  },
+  {
+    value: "Knex",
+    icon: <MdQueryBuilder />,
+  },
+  {
+    value: "SQLite3",
+    icon: <SiSqlite />,
+  },
+  {
+    value: "Next.JS",
+    icon: <SiNextdotjs />,
+  },
+  {
+    value: "Sass",
+    icon: <SiSass />,
+  },
+  {
+    value: "Prismic",
+    icon: <SiPrismic />,
+  },
+  {
+    value: "FaunaDB",
+    icon: <ImDatabase />,
+  },
+  {
+    value: "Stripe",
+    icon: <SiStripe />,
+  },
+  {
+    value: "Next Auth",
+    icon: <SiAuthy />,
+  },
+  {
+    value: "Styled Components",
+    icon: <SiStyledcomponents />,
+  },
+];
