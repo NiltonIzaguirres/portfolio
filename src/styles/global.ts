204c71c24c2a15a9f8a1973b571b2f6f087d7a1c
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2023-02-07 16:10:06 -0300

    style: style for scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -22,8 +22,8 @@ export const GlobalStyles = createGlobalStyle`
   html { scroll-behavior: smooth; }
 
   body {
-    background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text};
+    background: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors.text};
   }
 
   body, input, textarea, select, button {
@@ -38,4 +38,31 @@ export const GlobalStyles = createGlobalStyle`
     color: inherit;
     text-decoration: none;
   }
-`
+
+  ::-webkit-scrollbar {
+      width: 10px;
+      height: 10px;
+  }
+  ::-webkit-scrollbar-button {
+    width: 0px;
+    height: 0px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #A78BFA;
+    border: 0px none #ffffff;
+    border-radius: 50px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #8B5CF6;
+  }
+  ::-webkit-scrollbar-thumb:active {
+    background: #C55CF6;
+  }
+  ::-webkit-scrollbar-track {
+    background: ${props => props.theme.colors.primaryMedium};
+    border: 1px none #fff;
+  }
+  ::-webkit-scrollbar-corner {
+    background: transparent;
+  }
+`;
